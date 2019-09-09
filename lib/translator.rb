30340fca2d6c9027415ff9a32f934e7d14fe253a
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 17:41:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,12 +8,9 @@ def load_library(file)
   get_info = {get_meaning: {}, get_emoji: {}}
   
   emoticons_hash.each do |meaning, emoji|
-    get_info[:get_meaning][meaning] = meaning
-    get_info[:get_emoji][emoji] = emoticon
+    binding.pry
   end
   
-  get_info
-  binding.pry
 end
 
 def get_japanese_emoticon
